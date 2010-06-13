e3f00b331524e8b07b49c35c770b657fbb594449
ethan.jucovy <ethan.jucovy@d250b380-2c73-11df-8474-0f70896a4c6b>
2010-06-13 14:20:21 +0000

    use config
    git-svn-id: https://socialplanning-opencore.googlecode.com/svn/opencore_zinefarm/trunk@75 d250b380-2c73-11df-8474-0f70896a4c6b

--- opencore_zinefarm/paste.py
+++ opencore_zinefarm/paste.py
@@ -3,21 +3,27 @@ import os.path
 
 from zine.application import Zine
 
-from zine.opencoreglue import CustomRequestApp
-
+from opencore_zinefarm.opencoreglue import CustomRequestApp
+import webob.exc 
 class ZineFarm(object):
+    def __init__(self, zine_instances_directory):
+        self.zine_instances_directory = zine_instances_directory
+
     def __call__(self, environ, start_response):
+        # figure out which Zine instance to dispatch to
+        # based on special request header
         project = environ.get('HTTP_X_OPENPLANS_PROJECT')
         if not project: 
-            raise RuntimeError("wtf mate")
-        zine_data_base = '/tmp/zinefarm'
+            return webob.exc.HTTPNotFound("No blog found for project %s" % project)(environ, start_response)
         instance_folder = os.path.join(
-            zine_data_base, project)
+            self.zine_instances_directory, project)
         app = object.__new__(CustomRequestApp)
         from zine import _core
         _core._application = app
         app.__init__(instance_folder)
         return app(environ, start_response)
 
-def app_factory(*args, **kw):
-    return ZineFarm()
+def app_factory(zine_instances_directory=None, *args, **kw):
+    assert zine_instances_directory is not None and os.path.isdir(zine_instances_directory), \
+        "zine_instances_directory must be supplied and must be an existing directory"
+    return ZineFarm(zine_instances_directory)
